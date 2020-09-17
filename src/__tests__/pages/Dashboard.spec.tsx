c947938ee15a86457d03447024a71a66d6e9bcdb
Beroni <beronimagalhaes@gmail.com>
2020-09-17 19:00:53 -0300

    Dashboard Spec

---
+++ src/__tests__/pages/Dashboard.spec.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import Dashboard from '../../pages/Dashboard';
+
+const mockedHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
+    Link: ({ children }: { children: React.ReactNode }) => children,
+  };
+});
+
+jest.mock('react-redux', () => {
+  return {
+    useDispatch: () => {
+      return jest.fn();
+    },
+    useSelector: () => {
+      return { user: { name: 'Joao' } };
+    },
+  };
+});
+
+describe('Dashboard', () => {
+  beforeEach(() => {
+    mockedHistoryPush.mockClear();
+  });
+
+  it('should be able to go', async () => {
+    const { getByTestId } = render(<Dashboard />);
+
+    const divElement = getByTestId('product-link');
+
+    fireEvent.click(divElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard-product');
+    });
+  });
+
+  it('should be able to render de page', async () => {
+    const { getByTestId } = render(<Dashboard />);
+
+    const buttonElement = getByTestId('logoff-button');
+
+    fireEvent.click(buttonElement);
+
+    await waitFor(() => {
+      expect(mockedHistoryPush).toHaveBeenCalledWith('/');
+    });
+  });
+});
